cf49ad5cd4684af91c8ba6ed0e6aff1357cb1f6c
orwa337 <orwa.houry@outlook.de>
2017-11-04 00:41:13 +0100

    convert to class

--- main.js
+++ main.js
@@ -1,54 +1,44 @@
 $(document).ready(function() {
   console.log('Document is loaded.');
-  var current = $('#div1');
+  var current = $('content .div1');
   $('body').css('display', 'none');
   $('body').fadeIn('slow');
-  $('#div1').fadeIn('slow');
-  /*
-  function mycurrent($('li')) {
-    current.attr('id') = $('li').attr('class');
-  } 
+  $('content .div1').fadeIn('slow');
 
-  $(this).on('click' , function() {
-    mycurrent($(this));
-    $('div').hide();
-    current.fadeIn();
-  });
-  */
- 
-  $('.div1').on('click', function() {
-    current = $('#div1');
-    $('#div1').fadeIn();
-    $('#div2').hide();
-    $('#div3').hide();
+
+  $('ul .div1').on('click', function() {
+    current = $('content .div1');
+    $('content .div1').fadeIn();
+    $('content .div2').hide();
+    $('content .div3').hide();
   });
 
-  $('.div2').on('click', function() {
-    $('#div1').hide();
-    current = $('#div2');
-    $('#div2').fadeIn();
-    $('#div3').hide();
+  $('ul .div2').on('click', function() {
+    $('content .div1').hide();
+    current = $('content .div2');
+    $('content .div2').fadeIn();
+    $('content .div3').hide();
   });
 
-  $('.div3').on('click', function() {
-    $('#div1').hide();
-    $('#div2').hide();
-    current = $('#div3');
-    $('#div3').fadeIn();
+  $('ul .div3').on('click', function() {
+    $('content .div1').hide();
+    $('content .div2').hide();
+    current = $('content .div3');
+    $('content .div3').fadeIn();
   });
-  
+
   $(document).keyup(function(event) {
     var ch = String.fromCharCode(event.which);
 
     if (event.keyCode === 39) {
-      if (parseInt(current.attr("id").slice(3, 4)) < $('#navi ul').children().length) {
+      if (parseInt(current.attr("class").slice(3, 4)) < $('#navi ul').children().length) {
         current.hide();
         current.next().fadeIn();
         current = current.next();
       }
     }
     if (event.keyCode === 37) {
-      if (parseInt(current.attr("id").slice(3, 4)) > 1) {
+      if (parseInt(current.attr("class").slice(3, 4)) > 1) {
 
         current.hide();
         current.prev().fadeIn();
